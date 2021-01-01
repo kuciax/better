6b5bac4ef297d6748a3c74b04ba0ea778ab13205
Maciej Kucia <maciejkucia@MacBook-Pro-Maciej.local>
2021-01-01 20:46:40 +0100

    add thunk

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,6 +1,10 @@
-import { createStore, Store } from "redux";
+import { createStore, Store, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import todos, { IAction, initialiState, IState } from "./reducer";
 
-const store: Store<IState, IAction> = createStore(todos, initialiState);
+const store: Store<IState, IAction> = createStore(
+  todos,
+  applyMiddleware(thunk)
+);
 
 export default store;
